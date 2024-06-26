b8a7c40988b9b3eb33fcd69232b4b40be49c4b8b
Octavio Navarro <octavio.navarro@tec.mx>
2024-06-25 18:19:35 -0600

    Base class has a basic state management, animations, and can move to a target position.

--- Kill_lemmings/Scripts/Mob.gd
+++ Kill_lemmings/Scripts/Mob.gd
@@ -1,24 +1,51 @@
-class_name Mob extends CharacterBody2D
+class_name Mob 
+extends CharacterBody2D
 
-var mob_name = "Mob"
-var hp := 10
-var att := 1
-var def := 5
-var speed : float = 300
-var dead := false
+## Base class for the enemies
 
-@export var target : Vector2
-
-var _animations : Array[String] = ['Idle', 'Attack', 'Run']
-
+@export var mob_name = "Mob"
 @onready var _animated_sprite = $AnimatedSprite2D
+@export var target : Vector2
+@export var max_hp := 10
+
+enum state {Idle, Running, Attacking}
+
+var hp : int = 10:
+	set(value): hp = clamp(value, 0, max_hp)
+	
+var _current_state : state = state.Idle:
+	set(value):
+		_current_state = value
+		set_animation()
+		
+var att : int = 1
+var def : int = 5
+var speed : float = 300
+var dead : bool = false
+var on_target : bool = false
+var _animations : Array[String] = ['Idle', 'Run', 'Attack']
 
 func _ready():
-	_animated_sprite.play('Idle')
+	set_animation()
 	
-func take_damage(value):
+func _process(delta):
+	if !on_target:
+		if _current_state != state.Running:	_current_state = state.Running
+		move_towards_target()
+		if global_position.distance_to(target) <= 3:
+			on_target = true
+			_current_state = state.Idle
+
+func set_animation() -> void:
+	_animated_sprite.play(_animations[_current_state])
+	
+func move_towards_target() -> void:
+	var direction = global_position.direction_to(target)
+	velocity = direction * speed
+	move_and_slide()
+	
+func take_damage(value : int) -> void:
 	hp -= value
 	if hp <= 0:
-		hp = 0
 		dead = true
 	
