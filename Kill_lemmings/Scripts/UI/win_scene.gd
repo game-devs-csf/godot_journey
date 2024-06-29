856eb731ed80d00f68ad1c56a1db37e98c6baae8
paydelimon22 <valentina2212.gonzalez@gmail.com>
2024-06-28 21:27:38 -0600

    added win and loss scenes

--- Kill_lemmings/Scripts/UI/win_scene.gd
+++ Kill_lemmings/Scripts/UI/win_scene.gd
@@ -1,13 +1,9 @@
 extends Node2D
-
-@onready var animation_player = $CanvasLayer/Node2D/AnimationPlayer
-
 @onready var skeleton = $skeleton
+@onready var knight = $knight
 
 func _ready():
-	animation_player.play("Start")
-	
+	skeleton.play("death")
+
 func _on_button_pressed():
-	skeleton.play("play")
-	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file("res://Scenes/Play_scenes/horizontal_playground.tscn")
